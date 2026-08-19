353f8f3e232cc0e21ec4667aa558e78b856855d6
Lorenzo Marques <lorenzo.signorini@hotmail.com>
2026-08-19 01:41:25 +0000

    Configueação do JPA

---
+++ ecommerce/src/main/java/markuelaz/ecommerce/entidades/Categoria.java
@@ -0,0 +1,16 @@
+package markuelaz.ecommerce.entidades;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class Categoria {
+    private Short id;
+    private String nome;
+    private String descricao;
+
+
+}
